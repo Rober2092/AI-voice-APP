71eefb56a1e58157961785bc44c8995459f24ff9
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2023-02-07 15:29:54 -0800

    fixed code

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-import datetime
-import speech_recognition as sr
+import openai
 import pyttsx3
+import speech_recognition as sr
 
-def speak(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
+
+
+openai.api_key =  "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"
+
+engine = pyttsx3.init()
 
 r = sr.Recognizer()
+mic = sr.Microphone(device_index=1)
 
-with sr.Microphone() as source:
-    print("Listening...")
-    audio = r.listen(source)
 
-try:
-    text = r.recognize_google(audio)
-    print(f"You said: {text}")
-    if "hello" in text:
-        speak("Hello I am luna! How can I help you today?")
-    elif "time" in text:
-        speak("The current time is " + str(datetime.datetime.now().time()))
-    else:
-        speak("I'm sorry, I didn't understand what you said.")      
-except:
-    print("Sorry, I didn't understand that.")
+conversation = ""
+user_name = "You"
+bot_name = "LUNA"
 
+while True:
+    with mic as source:
+        print("\nlistening...")
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
+    print("no longer listening.\n")
+
+    try:
+        user_input = r.recognize_google(audio)
+    except:
+        continue
+
+    prompt = user_name + ": " + user_input + "\n" + bot_name+ ": "
+
+    conversation += prompt  # allows for context
+
+    # fetch response from open AI api
+    response = openai.Completion.create(engine='text-davinci-003', prompt=conversation, max_tokens=100)
+    response_str = response["choices"][0]["text"].replace("\n", "")
+    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
+
+    conversation += response_str + "\n"
+    print(response_str)
+
+    engine.say(response_str)
+    engine.runAndWait()
